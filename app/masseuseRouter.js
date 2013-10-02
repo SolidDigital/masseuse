0aa88d4f61342fef624b0e583f0263b23ef83462
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-10-02 08:55:47 -0700

    Router: moving router intialize into here

--- app/masseuseRouter.js
+++ app/masseuseRouter.js
@@ -11,5 +11,34 @@ define([
          * @extends Backbone.Router
          */
         return Backbone.Router.extend({
+            initialize : initialize,
+            _bindRoutes: noop
         });
+
+        function noop() {}
+
+        function initialize() {
+            var self = this;
+
+            if (this.beforeRouting) {
+                _(this.routes).chain()
+                    .omit(this.excludeFromBeforeRouting)
+                    .each(function (methodName) {
+                        var oldMethod = self[methodName];
+
+                        self[methodName] = function () {
+                            var args = arguments;
+
+                            this.beforeRouting()
+                                .done(function () {
+                                    oldMethod.apply(self, args);
+                                })
+                                .fail(function () {
+                                    self.beforeRoutingFailure.apply(self, args);
+                                });
+                        };
+                    });
+            }
+            Backbone.Router.prototype._bindRoutes.call(this);
+        }
     });
