b7037e79efc6b726719cb7cf0eafaad760980675
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-10-02 08:29:57 -0700

    Router beginnings

---
+++ app/masseuseRouter.js
@@ -0,0 +1,15 @@
+/*global define*/
+define([
+    'backbone',
+    'underscore',
+    'baseView',
+],
+    function (Backbone, _, BaseView) {
+
+        /**
+         * @class MasseuseRouter
+         * @extends Backbone.Router
+         */
+        return Backbone.Router.extend({
+        });
+    });
