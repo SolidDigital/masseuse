2c48e065f048dfb5872b7a02b4b5e0b8a4229e21
Jonathan Waltner <jonathan.waltner@thinksolid.com>
2013-12-09 15:34:37 -0800

    Refactored enclose to reuse more code and make ".closure" not required

--- app/utilities/enclose.js
+++ app/utilities/enclose.js
@@ -2,38 +2,52 @@
 define(function () {
     'use strict';
 
-    return function(method) {
-        return {
-            bindContext : function(context) {
-                return {
-                    closure : function() {
-                        method.apply(context, arguments);
-                    },
-                    withArgs : function() {
-                        var withArguments = arguments;
-                        return {
-                            closure : function() {
-                                method.apply(this, withArguments);
-                            }
-                        }
-                    }
-                }
-            },
-            withArgs : function() {
-                var withArguments = arguments;
-                return {
-                    closure : function() {
-                        method.apply(this, withArguments);
-                    },
-                    bindContext : function(context) {
-                        return {
-                            closure : function() {
-                                method.apply(context, withArguments);
-                            }
-                        }
-                    }
-                }
-            }
+    var publicApi = [
+        'withArgs',
+        'bindContext'
+    ];
+
+    function Closure (method) {
+        var self = this;
+
+        if (!(this instanceof Closure)) {
+            return new Closure(method);
         }
+
+        this.method = method;
+        this.context = undefined;
+        this.args = undefined;
+
+        this.closure = function () {
+            var args = (self.args || [])
+                    .concat(Array.prototype.slice.apply(arguments)),
+                context = self.context || this;
+
+            self.method.apply(context, args);
+        };
+
+        for(var i = 0; i < publicApi.length; ++i) {
+            (function (methodName) {
+                self.closure[methodName] = function () {
+                    return self[methodName].apply(self, arguments);
+                };
+            }(publicApi[i]));
+        }
+
+        this.closure.closure = this.closure;
+    }
+
+    Closure.prototype.withArgs = function () {
+        this.args = Array.prototype.slice.apply(arguments);
+
+        return this.closure;
     };
+
+    Closure.prototype.bindContext = function (context) {
+        this.context = context;
+
+        return this.closure;
+    };
+
+    return Closure;
 });
