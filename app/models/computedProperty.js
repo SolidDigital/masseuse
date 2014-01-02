444ce8b03a4d1621adcb650f099ac083740ca9bd
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-01-02 11:07:34 -0800

    jsdoc work and adding grunt js doc with first run of docs generation

--- app/models/computedProperty.js
+++ app/models/computedProperty.js
@@ -1,13 +1,15 @@
-/*global define:false*/
-define([], function () {
+define(function () {
     'use strict';
 
     /**
+     * A ComputedProperty is a property that depends on one or more other model values. Each time one of the other
+     * values is changed, the ComputeProperty callback is called with the values of all the dependent fields.
+     * See the tests for example usage.
+     * @constructor
+     * @alias module:ComputedProperty
      * @param listenablePropertyNames
      * @param callback
      * @param skipInitialComputation
-     * @returns {ComputedProperty}
-     * @constructor
      */
     function ComputedProperty (listenablePropertyNames, callback, skipInitialComputation) {
         if (!(this instanceof ComputedProperty)) {
