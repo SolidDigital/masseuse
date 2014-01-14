67f6f35f213c8bf53b3bb315038926f9c169fb82
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-01-14 13:21:35 -0800

    partial rivets test implementation

---
+++ tests/plugins/rivets/mainTest.js
@@ -0,0 +1,78 @@
+define(['jquery', 'underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'rivetsPlugin', 'sinonSpy'],
+    function ($, _, chai, mocha, sinon, sinonChai, masseuse, rivetsPlugin) {
+
+        'use strict';
+
+        var testDom = 'testDom',
+            riveted = 'riveted',
+            $body = $('body'),
+            BaseView = masseuse.BaseView,
+            template = '<div id="' + riveted + '">{{data.title}}</div>',
+            RivetView;
+
+        chai.use(sinonChai);
+        mocha.setup('bdd');
+
+
+        describe('view riveting', function () {
+            beforeEach(function() {
+                var $div = $('<div id="' + testDom + '"/>');
+                $('#' + testDom).remove();
+                $body.append($div);
+
+                RivetView = BaseView.extend({
+                    intialize: initialize,
+                    start: start
+                });
+            });
+
+            it('test dom is present', function() {
+                $('#' + testDom).length.should.equal(1);
+            });
+
+            it('test dom is riveted', function(done) {
+                var options = {
+                    el : '#' + testDom,
+                    viewOptions : ['rivetConfig'],
+                    rivetConfig : 'auto',
+                    templateHtml : template,
+                    modelData : {
+                        title : 'There it is.'
+                    }
+                };
+
+                window.options = options;
+                window.RV = RivetView;
+                new RivetView(options).start().done(function() {
+                    $('#' + riveted).html().should.equal('There it is.');
+                    done();
+                });
+            });
+        });
+
+        function initialize (options) {
+            console.log("-----");
+            options.plugins = [];
+            options.plugins.push(rivetsPlugin.plugin);
+            this.options = options;
+            BaseView.prototype.initialize.call(this, options);
+        }
+
+        function start () {
+            var $promise = BaseView.prototype.start.apply(this, arguments),
+                self = this;
+
+            $promise.progress(function (event) {
+                switch (event) {
+                case BaseView.afterRenderDone:
+                    if (self.rivetConfig) {
+                        self.rivetView();
+                    }
+                    break;
+                }
+            });
+
+            return $promise;
+        }
+
+    });
