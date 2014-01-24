6509792e66dbcd055442242355cb96ae1b69ece4
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-01-23 17:32:46 -0800

    Fixing merge bugs. Needed to add this.collection onto rivets.

--- app/plugins/rivets/adapters.js
+++ app/plugins/rivets/adapters.js
@@ -1,72 +1,79 @@
-define(['backbone'], function(Backbone) {
-    'use strict';
+/* jshint loopfunc:true, maxdepth:3 */
+define(['jquery', 'rivets', './configureMethod', 'backbone'],
+    function ($, Rivets, configureMethod, Backbone) {
+        'use strict';
 
-    var keySeparator = /->/g;
+        var keySeparator = /->/g;
 
-    return {
-        ':' :  {
-            /**
-             * @memberof adapter
-             * @instance
-             * @param model
-             * @param keypath
-             * @param callback
-             */
-            subscribe : function (model, keypath, callback) {
-                keypath = keypath.replace(keySeparator,'.');
-                if (model instanceof Backbone.Collection) {
-                    model.on('add remove reset refresh', function (obj, keypath) {
-                        callback(obj.get(keypath));
-                    });
-                } else if (model instanceof Backbone.Model) {
-                    model.on('change', function () {
-                        // TODO: make this more efficient / specific
-                        callback(model.get(keypath));
-                    });
-                }
-            },
+        /**
+         * @namespace adapter
+         */
+        return {
+            ':' :  {
 
-            /**
-             * @memberof adapter
-             * @instance
-             * @param model
-             * @param keypath
-             */
-            unsubscribe : function (model, keypath) {
-                if (typeof (model) == 'undefined') {
-                    return;
-                }
-                if (model instanceof Backbone.Collection) {
-                    model.off('add remove reset refresh');
-                } else if (model.off) {
-                    model.off('change:' + keypath.replace(keySeparator,'.'));
-                }
-            },
+                /**
+                 * @memberof adapter
+                 * @instance
+                 * @param model
+                 * @param keypath
+                 * @param callback
+                 */
+                subscribe : function (model, keypath, callback) {
+                    keypath = keypath.replace(keySeparator,'.');
+                    if (model instanceof Backbone.Collection) {
+                        model.on('add remove reset change', function (obj, keypath) {
+                            callback(obj.get(keypath));
+                        });
+                    } else if (model instanceof Backbone.Model) {
+                        model.on('change', function () {
+                            // TODO: make this more efficient / specific
+                            callback(model.get(keypath));
+                        });
+                    }
+                },
 
-            /**
-             * @memberof adapter
-             * @instance
-             * @param model
-             * @param keypath
-             * @returns {*}
-             */
-            read : function (model, keypath) {
-                return model.get(keypath.replace(keySeparator,'.'));
-            },
-            /**
-             * @memberof adapter
-             * @instance
-             * @param model
-             * @param keypath
-             * @param value
-             */
-            publish : function (model, keypath, value) {
-                if (model instanceof Backbone.Collection) {
-                    model[keypath.replace(keySeparator,'.')] = value;
-                } else if (model instanceof Backbone.Model) {
-                    model.set(keypath.replace(keySeparator,'.'), value);
+                /**
+                 * @memberof adapter
+                 * @instance
+                 * @param model
+                 * @param keypath
+                 */
+                unsubscribe : function (model, keypath) {
+                    if (typeof (model) == 'undefined') {
+                        return;
+                    }
+                    if (model instanceof Backbone.Collection) {
+                        model.off('add remove reset change');
+                    } else if (model.off) {
+                        model.off('change:' + keypath.replace(keySeparator,'.'));
+                    }
+                },
+
+                /**
+                 * @memberof adapter
+                 * @instance
+                 * @param model
+                 * @param keypath
+                 * @returns {*}
+                 */
+                read : function (model, keypath) {
+                    if(model instanceof Backbone.Collection) {
+                        return model.models;
+                    }
+                    return model.get(keypath.replace(keySeparator,'.'));
+                },
+                /**
+                 * @memberof adapter
+                 * @instance
+                 * @param model
+                 * @param keypath
+                 * @param value
+                 */
+                publish : function (model, keypath, value) {
+                    if (model instanceof Backbone.Model) {
+                        model.set(keypath.replace(keySeparator,'.'), value);
+                    }
                 }
             }
-        }
-    };
-});
+        };
+    });
