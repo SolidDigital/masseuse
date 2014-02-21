001b6f72cd23e473f818318ee2293806779f9cac
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-02-21 11:04:20 -0800

    One more pass.

--- tests/plugins/rivets/formattersTests.js
+++ tests/plugins/rivets/formattersTests.js
@@ -76,19 +76,25 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'check', '../../../
             });
 
             it('prettyDate makes a iso date string nicer looking', function() {
-                // phantom JS has issues parsing dates : https://github.com/ariya/phantomjs/issues/11151
-                var expected = typeof mochaPhantomJS === 'undefined' ? '12/21/2014 7:29:36 AM' : '12/21/2014 07:29:36',
-                    options = {
+                var options = {
                         timeZone : 'UTC',
                         timeZoneName : 'short'
                     },
                     date = new Date('2014-12-21T15:29:36.228Z');
-                expected;
+
                 check(formatters.prettyDate, [
                     [date.toUTCString(), date.toLocaleDateString('en-US', options) + ' ' +
                         date.toLocaleTimeString('en-US', _.extend({}, options, { hour12 : true}))]
                 ]);
-                //toLocaleDateString() + ' ' + date.toLocaleTimeString('en-US', {hour12: true})
+            });
+
+            it('prettyDate makes a iso date string nicer looking', function() {
+                var date = new Date('2014-12-21T15:29:36.228Z');
+
+                check(formatters.prettyDate, [
+                    [date.toUTCString(), date.toLocaleDateString('en-US') + ' ' +
+                        date.toLocaleTimeString('en-US', { hour12 : true})]
+                ]);
             });
 
             it('prettyDateNoTime returns a formatted date without the time', function() {
@@ -96,7 +102,6 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'check', '../../../
                     ['2014-12-21T15:29:36.228Z', '12/21/2014']
                 ]);
             });
-
 
             it('dollars formats a number as American Dollars', function() {
                 check(formatters.dollars, [
