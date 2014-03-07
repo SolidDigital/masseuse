ad15da80f6e4dca461ba10c885b2132474906900
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-03-06 22:47:16 -0800

    travis fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-function setupGitHooks(){
-    echo "Adding pre-commit hook."
-    cp hooks/pre-commit.sh .git/hooks/pre-commit &&
-    chmod +x .git/hooks/pre-commit
-}
-
-setupGitHooks
+echo "Adding pre-commit hook."
+cp hooks/pre-commit.sh .git/hooks/pre-commit &&
+chmod +x .git/hooks/pre-commit
 # good to go even if we errored out (e.g. hooks directory is not available)
 exit 0
