3a04d68deb9a7531298b2f19cce9de0b3b48635c
kaijarayne <kaija.rayne@thinksolid.com>
2014-03-11 11:58:00 -0700

    added tests for start method setting hasStarted and $startPromise

--- tests/views/lifeCycleTests.js
+++ tests/views/lifeCycleTests.js
@@ -72,20 +72,25 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
 
             //-----------Tests-----------
             describe('start method', function () {
+
                 it('should exist', function () {
                     should.exist(viewInstance.start);
                 });
+
                 it('should be a function', function () {
                     viewInstance.start.should.be.a('function');
                 });
+
                 it('should return a promise', function () {
                     var promise = viewInstance.start();
                     promise.should.have.property('done');
                     promise.should.not.have.property('resolve');
                 });
+
                 it('should return the same promise if called twice', function() {
                     viewInstance.start().should.equal(viewInstance.start());
                 });
+
                 it('should not run the life cycle methods if called twice', function() {
                     var events = [];
                     viewInstance.start();
@@ -97,6 +102,31 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
                     viewInstance.start();
                     events.length.should.equal(0);
                 });
+
+                it('should set the property hasStarted when startPromise is resolved', function(done) {
+                    viewInstance.start()
+                        .done(function() {
+                            viewInstance.should.have.property('hasStarted');
+                            done();
+                        });
+                });
+
+                it('should set the hasStarted property to true when startPromise is resolved', function(done) {
+                    viewInstance.start()
+                        .done(function() {
+                            viewInstance.hasStarted.should.equal(true);
+                            done();
+                        });
+                });
+
+                it('should set the property $startPromise when startPromise is resolved', function(done) {
+                    viewInstance.start()
+                        .done(function() {
+                            viewInstance.should.have.property('$startPromise');
+                            done();
+                        });
+                });
+
                 describe('promise', function () {
                     // Using done as a spy. If it is not called, the test will fail.
                     it('should be resolved after start promise is resolved', function (done) {
@@ -106,12 +136,14 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
                                 done();
                             });
                     });
+
                     it('should be resolved immediately after start is called if all implemented life cycle methods' +
                         'are synchronous', function () {
                         var $promise = viewInstance.start();
                         $promise.state().should.equal('resolved');
 
                     });
+
                     it('should be triggered with "beforeRenderDone", "renderDone", and "afterRenderDone" in sequence',
                         function () {
                             var eventSpy = sinon.spy();
@@ -122,6 +154,7 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
                             eventSpy.thirdCall.args[0].should.equal('renderDone');
                             eventSpy.getCall(3).args[0].should.equal('afterRenderDone');
                         });
+
                     it('view should have events "beforeRenderDone", "renderDone", and "afterRenderDone" in sequence',
                         function (done) {
                             var eventSpy = sinon.spy();
@@ -189,6 +222,7 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
                     });
 
                 });
+
                 describe('render method', function () {
                     it('should exist', function () {
                         should.exist(viewInstance.render);
@@ -230,6 +264,7 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
                 });
 
                 describe('remove method', function() {
+
                     it('should remove any children', function(done) {
                         var childView = new BaseView();
 
@@ -320,7 +355,6 @@ define(['underscore', 'chai', 'mocha', 'sinon', 'sinonChai', 'masseuse', 'sinonS
                         });
                 });
             });
-
 
             describe('refresh method', function() {
                 it('should not return the same promise as the start method', function() {
